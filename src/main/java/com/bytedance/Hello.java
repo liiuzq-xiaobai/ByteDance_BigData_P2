78696817ecaf27ea80960fb96d658f1871803ab6
曾博文 <kevin.zeng@rydeen.com.cn>
2022-08-04 17:28:50 +0800

    改为Maven项目

---
+++ src/main/java/com/bytedance/Hello.java
@@ -0,0 +1,12 @@
+package com.bytedance;
+
+/**
+ * @author kevin.zeng
+ * @description
+ * @create 2022-08-04
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
