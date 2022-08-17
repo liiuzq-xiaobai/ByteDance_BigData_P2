eb2fd61da4270af256a264d8d1133115af64315e
superM478 <952821478@qq.com>
2022-08-17 22:57:40 +0800

    sink算子保存数据到result
    当SinkStreamTask拉到record数据，将record数据放入SinkStreamTask的缓冲池（sinkBufferPool）；当SinkStreamTask拉到checkpoint数据，意味着需要保存缓冲池的数据到result。
    当第一个checkpoint到来时，缓冲池中的数据放入result，当第二个checkpoint到来时，先把result里的数据写入到文件，再将缓冲池的数据放入result

--- src/task/SinkStreamTask.java
+++ src/task/SinkStreamTask.java
@@ -1,5 +1,7 @@
 package task;
 
+import io.SinkBufferPool;
+import record.CheckPointBarrier;
 import record.StreamElement;
 import record.StreamRecord;
 import utils.SinkUtils;
@@ -15,6 +17,12 @@ import java.util.concurrent.TimeUnit;
  * @create 2022-08-14
  */
 public class SinkStreamTask<IN> extends StreamTask<IN,String> {
+    public SinkBufferPool result;
+
+    public SinkStreamTask(SinkBufferPool result) {
+        this.result = result;
+    }
+
     @Override
     public void run(){
         String name = Thread.currentThread().getName();
@@ -23,20 +31,43 @@ public class SinkStreamTask<IN> extends StreamTask<IN,String> {
             System.out.println(name + " read from InputChannel");
             StreamElement inputElement = this.input.take();
             //判断拉取数据的类型
+            //当SinkStreamTask拉到record数据，将数据输出到缓冲池
             if(inputElement.isRecord()){
                 StreamRecord<IN> inputRecord = inputElement.asRecord();
-                //写入文件
-                IN value = inputRecord.getValue();
-                try {
-                    SinkUtils.writeIntoFile("output/wordcount.txt",inputRecord);
-                } catch (IOException e) {
-                    e.printStackTrace();
+                output.add(inputElement);
+            //当SinkStreamTask拉到checkpoint数据，意味着需要保存缓冲池的数据到result，
+            } else if (inputElement.isCheckpoint()) {
+                //如果此时result中已有checkpoint，那么就先将result中的数据写入到output文件
+                if (result.isCheckpointExist()) {
+                    for (int i = 0; i < result.getList().size(); i++) {
+                        StreamElement input = (StreamElement) result.getList().get(i);
+                        if (input.isCheckpoint()) {
+                            result.getList().remove(input);
+                        } else if (input.isRecord()) {
+                            StreamRecord inputRecord = (StreamRecord) input;
+                            //写入文件，并删除result中已写过的数据
+                            IN value = (IN) inputRecord.getValue();
+                            try {
+                                SinkUtils.writeIntoFile("output/wordcount.txt",inputRecord);
+                                result.getList().remove((StreamElement) inputRecord);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            System.out.println(name + " 【print value】" + value);
+                        }
+                    }
                 }
-                System.out.println(name + " 【print value】" + value);
+                //如果result没有checkpoint，那么将缓冲池的数据保存到result，并且加入checkpoint以标记
+                CheckPointBarrier inputCheckpoint = inputElement.asCheckpoint();
+                output.add(inputElement);
+                result.copyExistingBuffer(output);
             }
             //放入当前Task的缓冲池，并推向下游（sink不需要推了）
 //            output.push(outputData,keySelector);
 //            System.out.println(name + " write into BufferPool");
         }
     }
+
+
 }
+
